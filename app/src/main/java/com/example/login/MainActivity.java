4d4a6a9bc66452163274a9bf037151fa6d4388aa
Arnoldo <arnold.aguirre91@gmail.com>
2024-09-09 16:48:08 -0300

    Activity Login terminada

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.login;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtmsj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtmsj=(TextView) findViewById(R.id.txtmensaje);
+        txtmsj.setText("Mensaje desde la capa controlador");
     }
 }
