39edcaaef0182640276507e915fa80ec21aea6cf
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-14 16:47:43 -0300

    Inicio de Activity para registrar medición

--- app/src/main/java/com/example/login/RegistroMedicion.java
+++ app/src/main/java/com/example/login/RegistroMedicion.java
@@ -1,6 +1,9 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistroMedicion extends AppCompatActivity {
+
+    // atributos
+    private TextView tipo_alumbrado_art5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,19 @@ public class RegistroMedicion extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Relacionar capas
+        tipo_alumbrado_art5 = (TextView) findViewById(R.id.tipo_alumbrado_art5);
+
+
+        // Configurar escuchador para ir a Activity tipo alumbrado art5
+        tipo_alumbrado_art5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ventana_art5 = new Intent(RegistroMedicion.this, TipoAlumbrado_art5.class);
+                startActivity(ventana_art5);
+            }
         });
     }
 }
